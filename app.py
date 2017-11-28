05d429aaa5fd711fc55e0a54a90df0e7a830743e
jeremy_woo <jeremy.woo@clearpoint.co.nz>
2017-11-28 17:59:29 +1300

    updated docker file and setup instruction.

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import braintree
-from flask import request, Flask
+from flask import request, Flask, url_for
 
 app = Flask(__name__)
-#app.run(host='0.0.0.0', port=1001)
+
 
 braintree.Configuration.configure(braintree.Environment.Sandbox,
 merchant_id="use_your_merchant_id",
@@ -10,8 +10,9 @@ public_key="use_your_public_key",
 private_key="use_your_private_key")
 
 @app.route('/')
+@app.route('/hello')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World docker!'
 
 @app.route('/client_token', methods=["GET"])
 def client_token():
@@ -20,3 +21,6 @@ def client_token():
 @app.route("/checkout", methods=["POST"])
 def checkout():
     client_nounce = request.form["payment_method_nonce"]
+       
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0', port=5001)
