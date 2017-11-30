e92c5866d6bc17d68a5768fb2af8055f7642636c
kepung <kepung@gmail.com>
2017-11-30 22:20:49 +1300

    added flask-sqlacademy

--- app.py
+++ app.py
@@ -4,11 +4,18 @@ from payment import paymentCore
 from tour.controllers import main
 from users.controllers import user
 import datetime
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
 app.register_blueprint(main, url_prefix='/tour')
 app.register_blueprint(user, url_prefix='/user')
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
 
 braintree.Configuration.configure(braintree.Environment.Sandbox,
 merchant_id="use_your_merchant_id",
