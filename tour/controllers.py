bf4c80e806499e65b86f0bce40dadcbee58a1f19
jeremy_woo <jeremy.woo@clearpoint.co.nz>
2017-11-30 13:14:06 +1300

    added location service tour.

--- tour/controllers.py
+++ tour/controllers.py
@@ -5,6 +5,10 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return "tour"
+@main.route('/<string:country>/<string:city>')
+def getByLocation(country, city):    
+    return "get location by" + country +  " " + city
+
 
 class SimpleApp():
     @staticmethod
