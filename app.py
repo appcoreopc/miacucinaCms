a06b6b050b8109b71aa1451d2b50994526bcdf53
jeremy_woo <jeremy.woo@clearpoint.co.nz>
2017-12-01 08:13:12 +1300

    tied up sqlite database and user model to j:/tmp/test.db.

--- app.py
+++ app.py
@@ -7,15 +7,36 @@ import datetime
 from flask_sqlalchemy import SQLAlchemy
 import os
 
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:////tmp/test.db', convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+
+
+Base = declarative_base()
+Base.query = db_session.query_property()
+from models import models
+
+
+def init_db():
+    # import all modules here that might define models so that
+    # they will be registered properly on the metadata.  Otherwise
+    # you will have to import them first before calling init_db()
+    from models import models
+    Base.metadata.create_all(bind=engine)
+
+
+
 app = Flask(__name__)
 
 app.register_blueprint(main, url_prefix='/tour')
 app.register_blueprint(user, url_prefix='/user')
-
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-db = SQLAlchemy(app)
-
 
 braintree.Configuration.configure(braintree.Environment.Sandbox,
 merchant_id="use_your_merchant_id",
@@ -27,7 +48,7 @@ private_key="use_your_private_key")
 def hello_world():
     a = paymentCore.BrainTreePayment()
     #b = SimpleApp()
-    return 'Hello, World docker2!:    ' + str(datetime.datetime.now())
+    return '  Hello, World docker2!:  ' + str(datetime.datetime.now())
 
 @app.route('/client_token', methods=["GET"])
 def client_token():
@@ -37,4 +58,8 @@ def client_token():
 def checkout():
     client_nounce = request.form["payment_method_nonce"]
        
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
 
+init_db()
