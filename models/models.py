b0057eba5fb2cf74b4a31c2b7c298334895140a6
jeremy_woo <jeremy.woo@clearpoint.co.nz>
2017-12-04 09:44:33 +1300

    updated models for cms.

--- models/models.py
+++ models/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, Numeric, DateTime
 from app import Base
 
 class User(Base):
@@ -7,68 +7,101 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(50), unique=False)
     email = Column(String(120), unique=True)
+    password = Column(String(50), unique=False)
 
-    def __init__(self, name=None, email=None):
+    def __init__(self, name=None, email=None, password= None):
         self.name = name
         self.email = email
+        self.password = password
 
     def __repr__(self):
         return '<User %r>' % (self.name)
 
 class Tour(Base):
-    __tablename__ = 'tour'
+    __tablename__ = 'tours'
     __table_args__ = {'extend_existing': True} 
+
     id = Column(Integer, primary_key=True)
     name = Column(String(50), unique=False)
     description = Column(String(120), unique=False)
     countryId = Column(Integer, unique=False)
     cityId = Column(Integer, unique=False)
-
-    def __init__(self, name=None, email=None):
+    price = Column(Numeric, unique=False)
+    paymentGateWay = Column(String(120), unique=False)
+    
+    def __init__(self, name=None, description=None):
         self.name = name
-        self.description = email
+        self.description = description
 
     def __repr__(self):
         return '<Tour %r>' % (self.name)
 
-class TourLocation(Base):
-    __tablename__ = 'tourlocation'
+class Location(Base):
+    __tablename__ = 'locations'
     id = Column(Integer, primary_key=True)
     name = Column(String(50), unique=True)
-    email = Column(String(120), unique=True)
+    description = Column(String(120), unique=True)
 
-    def __init__(self, name=None, email=None):
+    def __init__(self, name=None, description=None):
         self.name = name
-        self.email = email
+        self.description = description
+
+    def __repr__(self):
+        return '<TourLocation %r>' % (self.name)
+
+class TourLocation(Base):
+    __tablename__ = 'tourlocations'
+    id = Column(Integer, primary_key=True)
+    tourId = Column(Integer, unique=True)
+    locationId = Column(Integer, unique=True)
+    description = Column(String(120), unique=True)
+
+    def __init__(self, tourId=None, locationId=None, description=None):
+        self.tourId = tourId
+        self.locationId = locationId
+        self.description =  description
 
     def __repr__(self):
         return '<TourLocation %r>' % (self.name)
 
 class Country(Base):
-    __tablename__ = 'country'
+    __tablename__ = 'countries'
     id = Column(Integer, primary_key=True)
     name = Column(String(50), unique=True)
-    email = Column(String(120), unique=True)
+    description = Column(String(120), unique=True)
 
-    def __init__(self, name=None, email=None):
+    def __init__(self, name=None, description=None):
         self.name = name
-        self.email = email
+        self.description = description
 
     def __repr__(self):
         return '<Country %r>' % (self.name)
 
 class City(Base):
-    __tablename__ = 'city'
+    __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
     name = Column(String(50), unique=True)
     description = Column(String(120), unique=True)
 
+    def __init__(self, name=None, description=None):
+        self.name = name
+        self.description = description
+
+    def __repr__(self):
+        return '<City %r>' % (self.name)
+
+class PaymentGateWay(Base):
+    __tablename__ = 'paymentgateways'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True)
+    description = Column(String(120), unique=True)
+    
     def __init__(self, name=None, email=None):
         self.name = name
         self.email = email
 
     def __repr__(self):
-        return '<City %r>' % (self.name)
+        return '<Contact %r>' % (self.name)
 
 class Contact(Base):
     __tablename__ = 'contact'
