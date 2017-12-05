c972d9987b15c96a90c6056d932780a0f425e8dc
jeremy_woo <jeremy.woo@clearpoint.co.nz>
2017-12-05 17:29:47 +1300

    refactor code to remove un-used codes.

--- tour/controllers.py
+++ tour/controllers.py
@@ -1,14 +1,10 @@
 import braintree
 from flask import Blueprint, jsonify, Flask
 import json
-
-import collections
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from models.models import User, Tour, Location
-from sqlalchemy.ext.serializer import loads, dumps
 
 from app import db
 from utilserializer import serializeItem
@@ -21,8 +17,7 @@ def index():
 
 @main.route('/<int:tourid>', methods=["GET"])
 def getLocation(tourid):  
-    user = User("test4", "test4", "secret")
-    
+    user = User("test4", "test4", "secret")        
     try:
        r = User.query.filter_by(id=tourid).first()
        print(r)        
@@ -87,71 +82,5 @@ def testpopulate():
 @main.route('/testget', methods=["GET"])
 def testget():
     tlist = Tour.query.filter_by(id=1).first()
- 
-    #print(is_sequence(tlist))    
-    # for a in dir(tlist):  
-    #       if not a.startswith('_') and not a.isupper():   
-    #         avalue = getattr(tlist, a)
-    #         if not is_collection(avalue):       
-    #          print('\t%s : %s' % (a, getattr(tlist, a)))
-    #         else:                
-    #          print('collection item %s' % a)     
     v = serializeItem(tlist, True) 
-    print(v)
-    jsonify(v)
-    # print("tlisttlisttlist")
-    # print(is_collection(tlist))
-
-    # print('======getting itenaries type:')                 
-    # print(is_collection(tlist.itenaries))
-
-    #print(dir(tlist.itenaries))
-
-    #print(type(tlist.itenaries))
-
-    #for x in tlist.itenaries:
-         #print(json.dumps(x))
-         #print(x.name)
-    
     return jsonify(v);
-
-def is_sequence(arg):
-    return (not hasattr(arg, "strip") and
-            hasattr(arg, "__getitem__") or
-            hasattr(arg, "__iter__"))
-
-def is_collection(obj):
-    return isinstance(obj, collections.Sequence) and not isinstance(obj, str)
-
-methodList = ['append', 'query_class', 'serialize', 'clear', 'copy', 'count', 'query', 'index', 'insert', 'metadata', 'extend', 'remove', 'sort', 'reverse', 'pop']
-
-# def serializeItem(obj, printLevel=False): 
-#     list = []
-#     for a in dir(obj):    
-#           #if printLevel == True:
-#              #print(a)   
-#           if not a.startswith('_') and not a.isupper() and a not in methodList:   
-#             avalue = getattr(obj, a)
-#             if not is_collection(avalue):       
-#              #print('\t%s : %s' % (a, avalue))
-#              list.append("'%s' : '%s' " % (a, avalue))
-#             else:
-#               #print('list item %s' % a)
-#               subContent = ''
-#               subContent += ' %s:[' % (a)       
-
-#               sublist =[]
-#               for lv in avalue:             
-#                 sublist.append(serializeItem(lv))
-
-#               subContent += ", ".join(sublist)
-#                 #print(subContent)
-            
-#               subContent += ']'              
-#               list.append(subContent)
-   
-#     fstr = ", ".join(list)
-#     return '{' + fstr + '}';
-
-             
-             #print('collection item %s' % a)      
